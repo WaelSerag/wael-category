12ed8912af7f687d7b3366b10ab99740e05d39d6
Wael Serag <waelserag1@gmail.com>
2020-11-29 16:36:26 +0200

    Add files via upload

---
+++ src/ClockFacdecs.php
@@ -0,0 +1,21 @@
+<?php
+namespace wael\category;
+
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class ClockFacdecs extends Controller
+{
+    public static function clock()
+    {
+    	$arr =   include(base_path('config/set_clock.php'));
+    	return $arr;
+    }
+
+    public static function map()
+    {
+    	return view('map');
+    }
+}
